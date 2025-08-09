9031100d379f7d721d6338718aa42edc8048d2d2
sumantrai1 <raisumant201@gmail.com>
2025-08-09 16:50:03 +0000

    import export in react

---
+++ textutils/src/module1.mjs
@@ -0,0 +1,11 @@
+import myExName , {myName, myFrName, myGfName } from "./module2.mjs";
+
+
+console.log(myExName);
+
+console.log(myName);
+console.log(myFrName);
+console.log(myGfName);
+
+
+
---
+++ textutils/src/module2.mjs
@@ -0,0 +1,11 @@
+const myName = "Sumant Rai"
+const myExName = "Tripti pal"
+const myGfName = "Shubhngi Mishra"
+const myFrName = "Rajkumar Pal"
+
+
+export default myExName;
+
+export {myFrName}
+export {myGfName}
+export {myName}
