4e1432be01e18f89482a9ff6651067d44c490fec
sumantrai1 <raisumant201@gmail.com>
2025-08-10 09:34:57 +0000

    some changes

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,16 +1,20 @@
-import About from "./components/About"
+// import About from "./components/About"
 import Navbar from "./components/Navbar"
-// import TextForm from "./components/TextForm"
+import TextForm from "./components/TextForm"
+// import React, {useState} from "react";
 
 function App() {
+
+  // const [darkMode, setDarkMode] = useState(false);
+
   return (
     <>
     
- <Navbar title="TextUtils" aboutText= "About us"/>
+ <Navbar title="TextUtils" aboutText= "About us"  />
 
  <div className="container my-3">
-     {/* <TextForm heading="Enter the text to analyze"/> */}
-     <About/>
+     <TextForm heading="Enter the text to analyze"/>
+     {/* <About/> */}
  </div>
 
 
--- project1/src/components/TextForm.js
+++ project1/src/components/TextForm.js
@@ -31,6 +31,20 @@ function TextForm(props) {
 
     }
 
+    const handleCopy = function(){
+        console.log('I am copy');
+        let text = document.getElementById('myBox')
+        text.select()
+        text.setSelectionRange(0, 9999)
+        navigator.clipboard.writeText(text.value)
+        
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+    }
+
     return (
         <>
             <div className='container'>
@@ -41,6 +55,8 @@ function TextForm(props) {
                 <button className="btn btn-primary mx-2" onClick={handleUpClick} >Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2 " onClick={handleLowClick} >Convert to Lowercase</button>
                 <button className="btn btn-primary mx-2 " onClick={handleClearText} >Clear Text</button>
+                <button className="btn btn-primary mx-2 " onClick={handleCopy} >Copy Text</button>
+                <button className="btn btn-primary mx-2 " onClick={handleExtraSpaces} >Remove extra space</button>
             </div>
 
             <div className="container my-3" >
